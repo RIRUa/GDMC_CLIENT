d7ec7d05307ff22a4ee22a03ec06c2ec5fb6129f
“RIRUa” <is0531si@ed.ritsumei.ac.jp>
2021-12-19 13:19:42 +0900

    必要そうな関数、変数の定義

--- util/Minecraft.cpp
+++ util/Minecraft.cpp
@@ -6,3 +6,27 @@
 //
 
 #include "Minecraft.hpp"
+
+std::string Minecraft::getMinecraftBlockName(MinecraftBlock block, std::string addition) {
+    
+    std::string blockName = "minecraft:";
+    
+    // static_cast<int>でMinecraftBlock->intのキャストを行う
+    switch (block) {
+        case static_cast<int>(Minecraft::MinecraftBlock::stone):
+            blockName += "stone";
+            break;
+            
+        default:
+            blockName += "air";
+            break;
+    }
+    
+    if (addition != std::string()) {
+        blockName += "[";
+        blockName += addition;
+        blockName += "]";
+    }
+    
+    return blockName;
+}
--- util/Minecraft.hpp
+++ util/Minecraft.hpp
@@ -8,6 +8,29 @@
 #ifndef Minecraft_hpp
 #define Minecraft_hpp
 
-#include <stdio.h>
+#include <iostream>
+
+
+namespace Minecraft {
+
+    enum class MinecraftBlock:int {
+        stone,
+        cobbleStone
+    };
+
+    std::string getMinecraftBlockName(MinecraftBlock block, std::string addition);
+
+    using position = int64_t;
+    
+    typedef struct __blockInfo {
+        position x;
+        position y;
+        position z;
+        MinecraftBlock block;
+        
+        std::string getInfo();
+    }blockInfo;
+}
+
 
 #endif /* Minecraft_hpp */
