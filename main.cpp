4bc779b0b1659e481661a16dc07a81e240389d68
“RIRUa” <is0531si@ed.ritsumei.ac.jp>
2022-06-15 10:16:11 +0900

    main関数に引数を追加し、中心座標を取れるようにした

--- main.cpp
+++ main.cpp
@@ -5,9 +5,20 @@
 
 #include "Procession/process.hpp"
 
-int main(){
+int main(int argc, char *argv[]){
     
-    WN::Vec3 center = WN::Vec3(0,220,0);
+    int x = std::atoi(argv[1]);
+    int z = std::atoi(argv[2]);
+    
+    int size_x = std::atoi(argv[3]);
+    int size_z = std::atoi(argv[4]);
+    
+    WN::Vec3 center = WN::Vec3(
+                               (x + size_x) / 2,
+                               220,
+                               (z + size_z) / 2
+                               );
+    std::cout << "center position is (" << center.x << ", " << center.z << ")" << std::endl;
     Process p = Process(&center);
     p();
     
